c02951483eeb485a55e11c092a8fc34589285662
pjotr-kerssies <pjotrpoke@gmail.com>
2024-11-04 15:36:08 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("reserveerBtn").addEventListener("click", function() {
+    document.getElementById("confirmMessage").innerText = "Je reservering is bevestigd! We kijken uit naar je komst!";
+});
+
